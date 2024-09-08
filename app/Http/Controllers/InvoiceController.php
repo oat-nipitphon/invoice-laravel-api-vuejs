2bd45c950b89515e68fff8b8b9afdb4bac6d0412
HelloAmaZingWorld <hello.ama2ing.w0r1d237@gmail.com>
2024-09-08 21:47:44 +0700

    update

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -14,157 +14,225 @@ class InvoiceController extends Controller
 {
 
     public function getInvoices () {
-        $invoices = Invoice::with('customer')->orderBy('id', 'ASC')->get();
-        // $products = Product::orderBy('id', 'ABC')->get();
-        // return $products;
-        return response()->json([
-            'invoices' => $invoices
-        ],200);
+        try {
+            // ASC น้อยไปมาก DESC มากไปน้อย
+            $invoices = Invoice::with('customer')->orderBy('id', 'ASC')->get();
+            return response()->json([
+                'invoices' => $invoices,
+                'magess' => 'Get Invoices Successfully.'
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'error' => 'Get Invoices Error Success : ' . $e->getMessage(),
+            ], 500);
+        }
     }
 
     public function searchInvoices (Request $request) {
+        try {
+            $search = $request->get('id');
+            if($search != null){
+                $invoices = Invoice::with('customer')
+                ->where('id', 'LIKE', "%$search%")
+                ->get();
 
-        $search = $request->get('id');
-        if($search != null){
-            $invoices = Invoice::with('customer')
-            ->where('id', 'LIKE', "%$search%")
-            ->get();
+                return response()->json([
+                    'invoices' => $invoices,
+                    'magess' => 'Search Get Invoices Successfully.'
+                ], 200);
+
+            }else{
+                return $this->getInvoices();
+            }
+
+        } catch (\Exception $e) {
             return response()->json([
-                'invoices' => $invoices,
-            ],200);
-        }else{
-            return $this->getInvoices();
+                'status' => 500,
+                'error' => 'Search Get Invoices Error Success : ' . $e->getMessage(),
+            ], 500);
         }
     }
 
     public function showGetInvoice ($id) {
+        try {
 
-        $invoice = Invoice::with('customer','InvoiceItem.product')->find($id);
-        // return $invoice;
-        return response()->json([
-            'invoice' => $invoice
-        ],200);
+            $invoice = Invoice::with('customer','InvoiceItem.product')->find($id);
+            return response()->json([
+                'invoice' => $invoice,
+                'magess' => 'Get Cuntomer Successfully.'
+            ], 200);
 
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'error' => 'Get Cuntomer Error Success. : ' . $e->getMessage(),
+            ], 500);
+        }
     }
 
-
     public function createInvoice (Request $request) {
+        try {
+            $counter = Counter::where('key', 'invoice')->first();
+            $random = Counter::where('key', 'invoice')->first();
+            $invoice = Invoice::orderBy('id', 'DESC')->first();
 
-        $counter = Counter::where('key', 'invoice')->first();
-        $random = Counter::where('key', 'invoice')->first();
-        $invoice = Invoice::orderBy('id', 'DESC')->first();
+            if($invoice){
+                $invoice = $invoice->id+1;
+                $counters = $counter->value + $invoice;
+            }else{
+                $counters = $counter->value;
+            }
 
-        if($invoice){
-            $invoice = $invoice->id+1;
-            $counters = $counter->value + $invoice;
-        }else{
-            $counters = $counter->value;
+            $formData = [
+                'number' => $counter->prefix.$counters,
+                'customer' => null,
+                'customer_id' => null,
+                'date' => date('Y-m-d'),
+                'due_date' => null,
+                'reference' => null,
+                'discount' => 0,
+                'term_and_conditions' => 'Default Terms and Conditions',
+                'item' => [
+                    'product_id' => null,
+                    'product' => null,
+                    'unit_price' => 0,
+                    'quantity' => 1
+                ],
+            ];
+
+            return response()->json([
+                'formData' => $formData,
+                'magess' => 'Insert Form New Invoice Connect Success.'
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'error' => 'An error occurred: ' . $e->getMessage(),
+            ], 500);
         }
-
-        $formData = [
-            'number' => $counter->prefix.$counters,
-            'customer' => null,
-            'customer_id' => null,
-            'date' => date('Y-m-d'),
-            'due_date' => null,
-            'reference' => null,
-            'discount' => 0,
-            'term_and_conditions' => 'Default Terms and Conditions',
-            'item' => [
-                'product_id' => null,
-                'product' => null,
-                'unit_price' => 0,
-                'quantity' => 1
-            ],
-        ];
-
-        return response()->json($formData);
-
     }
 
     public function createInvoiceConfig (Request $request) {
-
-        $invoiceitem = $request->input("invoice_item");
-        $invoicedata['sub_total'] = $request->input("subtotal");
-        $invoicedata['total'] = $request->input("total");
-        $invoicedata['customer_id'] = $request->input("customer_id");
-        $invoicedata['number'] = $request->input("number");
-        $invoicedata['date'] = $request->input("date");
-        $invoicedata['due_date'] = $request->input("due_date");
-        $invoicedata['discount'] = $request->input("discount");
-        $invoicedata['reference'] = $request->input("reference");
-        $invoicedata['terms_and_conditions'] = $request->input("terms_and_conditions");
-        $invoice = Invoice::create($invoicedata);
-
-        foreach (json_decode($invoiceitem) as $item) {
-            $itemdata['product_id'] = $item->id;
-            $itemdata['invoice_id'] = $invoice->id;
-            $itemdata['quantity'] = $item->quantity;
-            $itemdata['unit_price'] = $item->unit_price;
-
-            InvoiceItem::create($itemdata);
+        try {
+            if($request->input("invoice_item")){
+                if(isset($request)){
+                    $invoicedata = [
+                        'sub_total'=> $request->input("subtotal"),
+                        'total'=> $request->input("total"),
+                        'customer_id'=> $request->input("customer_id"),
+                        'number'=> $request->input("number"),
+                        'date'=> $request->input("date"),
+                        'due_date'=> $request->input("due_date"),
+                        'discount'=> $request->input("discount"),
+                        'reference'=> $request->input("reference"),
+                        'terms_and_conditions'=> $request->input("terms_and_conditions"),
+                    ];
+                    $invoice = Invoice::create($invoicedata);
+                }
+                if($invoiceitem = $request->input("invoice_item")){
+                    foreach (json_decode($invoiceitem) as $item) {
+                        $itemdata = [
+                            'product_id' => $item->id,
+                            'invoice_id' => $invoice->id,
+                            'quantity' => $item->quantity,
+                            'unit_price' => $item->unit_price,
+                        ];
+                        InvoiceItem::create($itemdata);
+                    }
+                }
+            }
+            return response()->json([
+                'status' => 200,
+                'megess' => 'Insert Invoice and Invoiceitem Success.'
+            ],200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'error' => 'An error occurred: ' . $e->getMessage(),
+            ], 500);
         }
-
-        return response()->json([
-            'status' => 200,
-            'megess' => 'Insert Invoice and Invoiceitem Success.'
-        ],200);
-
     }
 
     public function cartItemDeleteInvoiceItem($id){
-        $invoice_item = InvoiceItem::findOrFail($id);
-        $invoice_item->delete();
+        try {
+            $invoice_item = InvoiceItem::findOrFail($id);
+            $invoice_item->delete();
+            return response()->json([
+                'status' => 200,
+                'megess' => 'Insert Invoice and Invoiceitem Success.'
+            ],200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'error' => 'An error occurred: ' . $e->getMessage(),
+            ], 500);
+        }
     }
 
     public function updateSqlEditInvoice(Request $request, $id){
+        try {
+            $invoice = Invoice::where('id', $id)->first();
+            $invoice_item = $request->input('invoice_item');
+            if(isset($invoice)){
+                $invoice = [
+                    'sub_total' => $request->subtotal,
+                    'total' => $request->total,
+                    'customer' => $request->customer,
+                    'number' => $request->number,
+                    'date' => $request->date,
+                    'due_date' => $request->due_date,
+                    'discount' => $request->discount,
+                    'refernce' => $request->refernce,
+                ];
+                $invoice->update($request->all());
+            }
+            if(isset($invoice_item)){
 
-        $invoice = Invoice::where('id', $id)->first();
+                $invoice->cartItemDeleteInvoiceItem()->delete();
+                foreach($invoice_item as $item){
+                    $itemdata = [
+                        'product_id' => $item->product_id,
+                        'invoice_id' => $invoice->id,
+                        'quantity' => $item->quantity,
+                        'unit_price' => $item->unit_price,
+                    ];
+                    InvoiceItem::create($itemdata);
+                }
+            }
+            return response()->json([
+                'status' => 200,
+                'megess' => 'Update Invoice and Invoiceitem Success.'
+            ],200);
 
-        $invoice->sub_total = $request->subtotal;
-        $invoice->total = $request->total;
-        $invoice->customer = $request->customer;
-        $invoice->number = $request->number;
-        $invoice->date = $request->date;
-        $invoice->due_date = $request->due_date;
-        $invoice->discount = $request->discount;
-        $invoice->refernce = $request->refernce;
-        $invoice->update($request->all());
-
-        $invoice_item = $request->input('invoice_item');
-        $invoice->cartItemDeleteInvoiceItem()->delete();
-
-        foreach($invoice_item as $item){
-
-            $itemdata['product_id'] = $item->product_id;
-            $itemdata['invoice_id'] = $invoice->id;
-            $itemdata['quantity'] = $item->quantity;
-            $itemdata['unit_price'] = $item->unit_price;
-            InvoiceItem::create($itemdata);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'error' => 'An error occurred: ' . $e->getMessage(),
+            ], 500);
         }
-
-        return response()->json([
-            'status' => 200,
-            'megess' => 'Update Invoice and Invoiceitem Success.'
-        ],200);
     }
 
     public function deleteInvoice($id){
-
-        $invoice = Invoice::find($id);
-
-        if ($invoice) {
-            // ลบรายการที่เกี่ยวข้องในตาราง InvoiceItem ก่อน
-            InvoiceItem::where('invoice_id', $id)->delete();
-
-            // ลบใบแจ้งหนี้ (Invoice)
-            $invoice->delete();
+        try {
+            $invoice = Invoice::find($id);
+            if (isset($invoice)) {
+                InvoiceItem::where('invoice_id', $id)->delete();
+                $invoice->delete();
+            }
 
             return response()->json([
-                'message' => 'Invoice and related items deleted successfully'
-            ], 200);
-        } else {
-            return response()->json(['message' => 'Invoice not found'], 404);
+                'status' => 200,
+                'megess' => 'Delete Invoice and Invoiceitem Success.'
+            ],200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'error' => 'An error occurred: ' . $e->getMessage(),
+            ], 500);
         }
     }
 
